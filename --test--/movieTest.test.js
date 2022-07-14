6ad7f49d178e52a39b6f6e443a96f58612f7c541
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2022-07-14 07:20:57 -0400

    eliminated comments on test file movieTest.test.js

--- --test--/movieTest.test.js
+++ --test--/movieTest.test.js
@@ -18,14 +18,3 @@ test("addFriendsWith should be a function", function(){
 test("getMovieWithHighestLikesAmongFriends should be a function", function(){
     expect(typeof newFriend.getMovieWithHighestLikesAmongFriends).toBe("function");
 });
-
-// test("addFriend() is called with arguments", function(){
-//     //Preparing a Spy for newFriend.addFriend() method 
-//     const newFriendSpy = jest.spyOn(newFriend, "addFriend");
-
-//     //Creating a mock rule for use as a function argument;
-//     const trueRule = jest.fn(()=>true);
-//     expect(newFriendSpy).toHaveBeenCalled("value", ["true"]);
-//     setRuleSpy.mockClear();
-
-// })
